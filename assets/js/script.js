8c9b798ddcc543bdcb1d0b27775ffb5f953f2eef
Farhana Howlader Farid <farhana039.fa@gmail.com>
2024-08-20 21:47:54 +0000

    Adding function to js

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
+    runGame("addition");
+});
+
+function runGame(gameType) {
+    
+}
+
+function checkAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
